e9adab89be583ddbf2297b45f13d9ed760e4ddb5
sergdon <sergdon76@gmail.com>
2023-03-04 00:37:27 +0300

    Update README.md

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SalesManager salesManager = new SalesManager();
+        int maxSale = salesManager.max(400);
+        System.out.println(maxSale);
+    }
+}
