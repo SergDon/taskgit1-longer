0ab6a1edc3685d7c72b2f8662286193a5d722c50
sergdon <sergdon76@gmail.com>
2023-03-04 00:37:27 +0300

    Update README.md

---
+++ src/SalesManager.java
@@ -0,0 +1,18 @@
+public class SalesManager {
+
+    protected long[] sales = {150, 200, 400};
+
+    public SalesManager() {
+        this.sales = sales;
+    }
+
+    public int max(long sales) {
+        long max = -1;
+        for (long sale : this.sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+}
