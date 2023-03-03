a886a71fbe6a5ac08817cfdda4551438d9ee62bb
SergDon <126449105+SergDon@users.noreply.github.com>
2023-03-04 00:34:31 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        SalesManager salesManager = new SalesManager();
-        int maxSale = salesManager.max(400);
-        System.out.println(maxSale);
-    }
-}
