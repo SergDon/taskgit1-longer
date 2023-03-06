199dac98d3ca75035572a0429b360d4568a80a37
SergDon <126449105+SergDon@users.noreply.github.com>
2023-03-06 11:13:11 +0300

    Delete SalesManager.java

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,42 +1,18 @@
 public class SalesManager {
 
-    protected int[] sales = {150, 200, 300, 400, 500, 600};
+    protected long[] sales = {150, 200, 400};
 
     public SalesManager() {
         this.sales = sales;
     }
 
-
-    public int max() {
-        int max = -1;
-        for (int sale : this.sales) {
-
     public long max(long sales) {
         long max = -1;
         for (long sale : this.sales) {
-
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
-    }
-
-
-    public int min() {
-        int min = Integer.MAX_VALUE;
-        for (int sale : sales) {
-            if (min > sale) {
-                min = sale;
-            }
-        }
-        return min;
-    }
-    public int average() {
-        int sum = 0;
-        for (int sale : sales) {
-            sum += sale;
-        }
-        return (sum - (min() + max())) / (sales.length - 2);
     }
 }
