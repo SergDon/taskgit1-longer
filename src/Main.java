2d934b9fdaa7f00d082087437b604272a9d80d8b
SergDon <126449105+SergDon@users.noreply.github.com>
2023-03-06 15:40:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         SalesManager salesManager = new SalesManager();
-        int maxSale = salesManager.max(300);
+        int maxSale = salesManager.max(400);
         System.out.println(maxSale);
     }
 }
