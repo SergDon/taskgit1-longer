d89d4fb303b7f483932eb13353562e5e3912b374
sergdon <sergdon76@gmail.com>
2023-03-07 08:51:20 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         SalesManager salesManager = new SalesManager();
-        int maxSale = salesManager.max(300);
+        int maxSale = salesManager.max();
+        int minSale = salesManager.min();
+        int average = salesManager.average();
         System.out.println(maxSale);
+        System.out.println(minSale);
+        System.out.println(average);
     }
 }
